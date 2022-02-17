9ecd3e1f4a417737f9cf1f5e8a5ae9ccb572eee0
Kewin Tao Anh <kewin.taoanh@gmail.com>
2022-02-17 23:08:27 +0100

    Przeniesienie starych programów

---
+++ hangman.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace hangman
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a secret word: ");
+            string word = Console.ReadLine();
+            char[] hidden = new char[word.Length];
+            for (int i = 0; i < word.Length; i++)
+                hidden[i] = '_';
+
+            while (true)
+            {
+                char playerGuess;
+                bool correctValue;
+                do
+                {
+                    Console.Write("Letter: ");
+                    correctValue = (char.TryParse(Console.ReadLine(), out playerGuess));
+                    if (hidden.Contains(playerGuess))
+                        Console.WriteLine("Choose another !");
+                    else if (word.Contains(playerGuess))
+                        Console.WriteLine("Correct!");
+                    else
+                        Console.WriteLine("Wrong...");
+
+                } while (!correctValue);
+
+                for (int j = 0; j < word.Length; j++)
+                {
+                    if (playerGuess == word[j])
+                        hidden[j] = playerGuess;
+                }
+
+
+                if (!hidden.Contains('_') || playerGuess.ToString() == word)
+                {
+                    Console.WriteLine("You won :D");
+                    break;
+                }
+
+                Console.WriteLine(hidden);
+            }
+        }
+    }
+}
---
+++ todo_list.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace todo_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> todoList = new List<string>();
+            todoList.Add("zjesc sniadanie");
+            todoList.Add("zjesc obiad");
+            todoList.Add("zjesc kolacje");
+
+            string opcja, zadanie;
+            int miejsce, dlugosc = 0;
+
+            Console.WriteLine("1 - Co chcesz dziś zrobić ?");
+            Console.WriteLine("0 - Odznacz wykonane :)");
+            Console.WriteLine("---------------------------");
+            dlugosc = todoList.Count;
+
+            do
+            {
+                Console.WriteLine("Ilość elementów: " + dlugosc);
+                // podaj cyfre 1 albo 0
+                Console.Write("> ");
+                opcja = Console.ReadLine();
+                // jak 1 - spytaj o zadanie i miejsce gdzie je dodac (zadbaj o sprawdzenie dlugosci listy!)
+                if (opcja == "1")
+                {
+                    Console.Write("Wpisz zadanie: ");
+                    zadanie = Console.ReadLine();
+                    dlugosc++;
+                    do
+                    {
+                        Console.Write("Jak ważne jes to dla Ciebie zadanie w skali  1 - {0}? ", dlugosc);
+                        miejsce = int.Parse(Console.ReadLine());
+                    } while ((miejsce < 1) || (miejsce > dlugosc));
+
+                    todoList.Insert(miejsce - 1, zadanie);
+                }
+                // jak 0 - spytaj o zadanie - usun to zadanie (nie pytaj jesli nie ma co usuwac!)
+                else if (opcja == "0")
+                {
+                    if (dlugosc == 0)
+                    {
+                        Console.WriteLine("Aktualnie nie ma żadnych zadań na Twojej liście.");
+                    }
+                    else
+                    {
+                        do
+                        {
+                            Console.Write("Podaj numer zadania do usunięcia: ");
+                            miejsce = int.Parse(Console.ReadLine());
+                        } while ((miejsce < 1) || (miejsce > dlugosc));
+                        todoList.Remove(todoList[miejsce - 1]);
+                        dlugosc--;
+                    }
+                }
+
+                // inne
+                else
+                    Console.WriteLine("Nie rozumiem :(");
+                // wyświetl zadania
+                int licznik = 0;
+                Console.WriteLine("------------------------------------------------------");
+                foreach (string zad in todoList)
+                {
+                    licznik++;
+                    Console.WriteLine(licznik + "." + zad);
+                }
+                Console.WriteLine("------------------------------------------------------");
+            } while (true);
+        }
+    }
+}
