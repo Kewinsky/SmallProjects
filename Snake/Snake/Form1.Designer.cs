428bfeefb17da40353a47bfd20388d47daaaead9
Kewin Tao Anh <kewin.taoanh@gmail.com>
2022-02-21 23:24:01 +0100

    Start gry. Generowanie obiektów. Aktualizacja ekranu.

--- Snake/Snake/Form1.Designer.cs
+++ Snake/Snake/Form1.Designer.cs
@@ -29,11 +29,66 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pbCanvas = new System.Windows.Forms.PictureBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbCanvas
+            // 
+            this.pbCanvas.Location = new System.Drawing.Point(12, 12);
+            this.pbCanvas.Name = "pbCanvas";
+            this.pbCanvas.Size = new System.Drawing.Size(426, 426);
+            this.pbCanvas.TabIndex = 0;
+            this.pbCanvas.TabStop = false;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblScore.Location = new System.Drawing.Point(444, 12);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(106, 45);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "Score:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(556, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 45);
+            this.label1.TabIndex = 2;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pbCanvas);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.pbCanvas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCanvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private PictureBox pbCanvas;
+        private Label lblScore;
+        private Label label1;
+        private System.Windows.Forms.Timer gameTimer;
     }
 }
--- Snake/Snake/Form1.cs
+++ Snake/Snake/Form1.cs
@@ -2,9 +2,56 @@ namespace Snake
 {
     public partial class Form1 : Form
     {
+        private List<Circle> Snake = new List<Circle> ();
+        private Circle food = new Circle ();
         public Form1()
         {
             InitializeComponent();
+
+            //Set settings to default
+            new Settings();
+
+            // Set game speed and start timer
+            gameTimer.Interval = 1000 / Settings.Speed;
+            gameTimer.Tick += UpdateScreen();
+            gameTimer.Start ();
+
+            // Start New Game
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            //Set settings to default
+            new Settings();
+
+            //Create new player object
+            Snake.Clear();
+            Circle head = new Circle();
+            head.X = 10;
+            head.Y = 5;
+            Snake.Add(head);
+
+            lblScore.Text = Settings.Score.ToString();
+            GenerateFood();
+
+        }
+
+        // Place random food
+        private void GenerateFood()
+        {
+            int maxXpos = pbCanvas.Size.Width/Settings.Width;
+            int maxYpos = pbCanvas.Size.Height/Settings.Height;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbCanvas_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
